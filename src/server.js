64f09b981637a0ab97eca02b083e7072563d68fc
Sarah <sarahvictoria230@gmail.com>
2023-07-15 17:33:13 -0300

    feat: Created  database connection
    Installed sqlite and created sqlite database connection

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require("express-async-errors")
 
 const express = require('express');
+const database = require("./database/sqlite")
 
 const routes = require("./routes");
 const AppError = require("./utils/appError");
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+database();
 
 app.use((error, request, response, next) => {
     if (error instanceof AppError) {
