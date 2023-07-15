04d5283caee8a93a7fdfea9838a8138b22c015e4
Sarah <sarahvictoria230@gmail.com>
2023-07-15 19:34:57 -0300

    feat: created user registration on database
    Created user registration on database and encrypted users password.

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,16 +1,24 @@
 const { hash } = require("bcryptjs")
-
 const appError = require("../utils/appError")
+const sqliteConnection = require("../database/sqlite")
 
 class UsersController {
-    create(request, response) {
+    async create(request, response) {
         const { name, email, password } = request.body;
 
-        if (!name) {
-            throw new appError("O nome é obrigatório!")
+        const database = await sqliteConnection();
+
+        const UserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        if (UserExists) {
+            throw new appError("Esse usuário já está cadastrado!")
         }
 
-        response.json({ name, email, password })
+        const hashedPassword = await hash(password, 8)
+
+        await database.run("INSERT INTO users (name, email,password) VALUES (?, ?, ?)", [name, email, hashedPassword]);
+
+        return response.json()
     }
 }
 
