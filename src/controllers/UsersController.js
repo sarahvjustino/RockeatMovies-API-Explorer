011e2074d9ac145dedde2ced25b69f6225bc268a
Sarah <sarahvictoria230@gmail.com>
2023-07-14 23:19:14 -0300

    feat: Created appError for exception handling
    Installed express-async-errors and did the expection handling logic

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,12 @@
+const appError = require("../utils/appError")
+
 class UsersController {
     create(request, response) {
         const { name, email, password } = request.body;
+
+        if (!name) {
+            throw new appError("O nome é obrigatório!")
+        }
 
         response.json({ name, email, password })
     }
--- src/server.js
+++ src/server.js
@@ -1,14 +1,30 @@
+require("express-async-errors")
+
 const express = require('express');
 
 const routes = require("./routes");
-
-
-
+const AppError = require("./utils/appError");
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use((error, request, response, next) => {
+    if (error instanceof AppError) {
+        return response.status(error.statusCode).json({
+            status: "Error",
+            message: error.message
+        })
+    }
+
+    console.log(error);
+
+    return response.status(500).json({
+        status: "Error",
+        message: "Internal server error"
+    })
+})
 
 const PORT = 3000;
 app.listen(PORT, () => `Server is running on PORT ${PORT}`);
