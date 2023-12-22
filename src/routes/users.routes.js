b7a9d6acbaa29c2b3632c51ead2ce0cabd339bb0
Sarah <sarahvictoria230@gmail.com>
2023-12-22 13:59:09 -0300

    feat: created upload image configs and DiskStorage

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,14 +1,18 @@
 const { Router } = require("express")
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
 const ensureAuthentication = require("../middlewares/ensureAuthentication");
 
-const UsersRoutes = Router()
+const usersRoutes = Router()
+const upload = multer(uploadConfig.MULTER)
 
 const usersController = new UsersController();
 
-UsersRoutes.post("/", usersController.create);
-UsersRoutes.put("/", ensureAuthentication, usersController.update);
-UsersRoutes.delete("/", ensureAuthentication, usersController.delete);
+usersRoutes.post("/", usersController.create);
+usersRoutes.put("/", ensureAuthentication, usersController.update);
+usersRoutes.delete("/", ensureAuthentication, usersController.delete);
+usersRoutes.patch("/avatar", ensureAuthentication,)
 
-module.exports = UsersRoutes;
+module.exports = usersRoutes;
