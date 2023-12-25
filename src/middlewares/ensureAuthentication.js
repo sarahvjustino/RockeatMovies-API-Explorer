a58f5035b53c7302c59dc32282a7aec853038484
Sarah <sarahvictoria230@gmail.com>
2023-12-25 17:30:07 -0300

    fix: fixed authentication middleware

--- src/middlewares/ensureAuthentication.js
+++ src/middlewares/ensureAuthentication.js
@@ -3,7 +3,7 @@ const AppError = require("../utils/AppError");
 const authConfig = require("../configs/auth");
 
 function ensureAuthentication(request, response, next) {
-    const authHeader = request.header.authorization;
+    const authHeader = request.headers.authorization;
 
     if (!authHeader) {
         throw new AppError("JWT token não informado", 401);
--- src/server.js
+++ src/server.js
@@ -4,12 +4,13 @@ const AppError = require("./utils/AppError");
 const UploadConfig = require("./configs/upload");
 
 const express = require('express');
-
 const routes = require("./routes");
+const cors = require("cors");
 
 migrationsRun()
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/files", express.static(UploadConfig.UPLOAD_FOLDER));
